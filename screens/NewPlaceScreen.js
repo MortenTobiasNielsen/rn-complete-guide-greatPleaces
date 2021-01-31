19182e7022856351d7984298f808eec51315d53a
Morten <mtnielsen89@gmail.com>
2021-01-31 09:34:02 +0100

    Added a simple form to add a new place and save it to redux

--- screens/NewPlaceScreen.js
+++ screens/NewPlaceScreen.js
@@ -1,11 +1,47 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  Button,
+  TextInput,
+  StyleSheet,
+  ScrollView,
+} from "react-native";
+import { useDispatch } from "react-redux";
+
+import Colors from "../constants/Colors";
+import * as placesAction from "../store/places-actions";
 
 const NewPlaceScreen = (props) => {
+  const [titleValue, setTitleValue] = useState("");
+
+  const dispatch = useDispatch();
+
+  const titleChangedHandler = (text) => {
+    setTitleValue(text);
+  };
+
+  const savePlaceHandler = () => {
+    dispatch(placesAction.addPlace(titleValue));
+    props.navigation.goBack();
+  };
+
   return (
-    <View>
-      <Text>NewPlaceScreen</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.form}>
+        <Text style={styles.label}>Title</Text>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={titleChangedHandler}
+          value={titleValue}
+        />
+        <Button
+          title="Save Place"
+          color={Colors.primary}
+          onPress={savePlaceHandler}
+        />
+      </View>
+    </ScrollView>
   );
 };
 
@@ -13,6 +49,21 @@ NewPlaceScreen.navigationOptions = {
   headerTitle: "Add Place",
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  form: {
+    margin: 30,
+  },
+  label: {
+    fontSize: 18,
+    marginBottom: 15,
+  },
+  textInput: {
+    borderBottomColor: "#ccc",
+    borderBottomWidth: 1,
+    marginBottom: 15,
+    paddingVertical: 4,
+    paddingHorizontal: 2,
+  },
+});
 
 export default NewPlaceScreen;
