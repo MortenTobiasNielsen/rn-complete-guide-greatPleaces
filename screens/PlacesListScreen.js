712386f43c372cafe137316f115a0f012df96cf7
Morten <mtnielsen89@gmail.com>
2021-01-31 09:57:34 +0100

    Added navigation to the details screen and a place item component

--- screens/PlacesListScreen.js
+++ screens/PlacesListScreen.js
@@ -1,21 +1,38 @@
 import React from "react";
-import { View, Text, StyleSheet, Platform } from "react-native";
+import { StyleSheet, Platform, FlatList } from "react-native";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import { useSelector } from "react-redux";
 
 import HeaderButton from "../components/HeaderButton";
+import PlaceItem from "../components/PlaceItem";
 
 const PlacesListScreen = (props) => {
+  const places = useSelector((state) => state.places.places);
+
   return (
-    <View>
-      <Text>PlacesListScreen</Text>
-    </View>
+    <FlatList
+      data={places}
+      renderItem={(itemData) => (
+        <PlaceItem
+          image={null}
+          title={itemData.item.title}
+          address={null}
+          onSelect={() => {
+            props.navigation.navigate("PlaceDetail", {
+              placeTitle: itemData.item.title,
+              placeId: itemData.item.id,
+            });
+          }}
+        />
+      )}
+    />
   );
 };
 
 PlacesListScreen.navigationOptions = (navData) => {
   return {
     headerTitle: "All Places",
-    headerRight: (
+    headerRight: () => (
       <HeaderButtons HeaderButtonComponent={HeaderButton}>
         <Item
           title="Add Place"
